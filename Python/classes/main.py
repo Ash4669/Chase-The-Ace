95835e3ab89538f414cda791c922f961149d0bbd
Ashley Whitehouse <ash4669@hotmail.co.uk>
2019-08-17 03:16:16 +0100

    Works and plays until one person has one life left. Don't know about draws yet but doesn't break when it tries to skip over unplaying players and hits the end of the array

--- Python/classes/main.py
+++ Python/classes/main.py
@@ -11,39 +11,54 @@ def main():
         playerList.append(Player(playerNames[i]))
         playerList[i].dealer = False
         playerList[i].lives = 3
+        playerList[i].outOfGame = False
+    while playerList[0].lives + playerList[1].lives + playerList[2].lives + playerList[3].lives + playerList[4].lives > 1:
 
-    while playerList[0].lives > 1 or playerList[1].lives > 1 or playerList[2].lives > 1 or playerList[3].lives > 1 or playerList[4].lives > 1:
+        noOneToTrade = False
 
         # setDealer
         playerList[len(playerList)-1].dealer = True
         Action.dealCards(playerList)
 
         for i in range(len(playerNames)):
-            print(playerList[i].name)
-            print(playerList[i].card)
+            print("Name: " + str(playerList[i].name) + ", card: " + str(playerList[i].card) + ", lives: " + str(playerList[i].lives))
+        print("")
         print(Card.outOfDeck)
 
         for i in range(len(playerNames)):
-            playerCurrent = playerList[i]
-            if i<4:
-                playerLeft = playerList[i+1]
+            if playerList[i].outOfGame == False:
+                playerCurrent = playerList[i]
+                if i < len(playerNames) - 1:
+                    while i < len(playerNames) - 1 and playerList[i+1].outOfGame == True:
+                        print("here")
+                        if i == len(playerNames) - 2:
+                            break
+                        i = i + 1
+                    if i == len(playerNames) - 2:
+                        noOneToTrade = True
+                    else:
+                        playerLeft = playerList[i+1]
 
-            # getPlayerChoice
-            playerCurrent.choice = "trade"
-            if playerCurrent.dealer == False:
-                Action.tradeOrStick(playerCurrent,playerLeft)
-            elif playerCurrent.dealer == True:
+                    if noOneToTrade == False:
+                        # getPlayerChoice
+                        playerCurrent.choice = "trade"
+                        if playerCurrent.dealer == False:
+                            Action.tradeOrStick(playerCurrent,playerLeft)
+                        elif playerCurrent.dealer == True:
 
-                # delete when getPlayerChoice written
-                playerCurrent.choice = "don't cut"
-                Action.cutTheDeck(playerCurrent)
+                            # delete when getPlayerChoice written
+                            playerCurrent.choice = "don't cut"
+                            Action.cutTheDeck(playerCurrent)
 
         Action.endRound(playerList)
 
         for i in range(len(playerNames)):
-            print(playerList[i].name)
-            print(playerList[i].card)
-            print(playerList[i].lives)
+            print("Name: " + str(playerList[i].name) + ", card: " + str(playerList[i].card) + ", lives: " + str(playerList[i].lives))
+        print("")
+
+        for player in playerList:
+            if player.lives == 0:
+                player.outOfGame = True
 
     Action.announceWinner(playerList)
 
