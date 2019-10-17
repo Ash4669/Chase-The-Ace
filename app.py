5f0f1be7ae355bdf82b1dd9c088e52ab755ebb10
Ashley Whitehouse <ash4669@hotmail.co.uk>
2019-10-17 23:27:19 +0100

    no progress. Just tidying

--- app.py
+++ app.py
@@ -22,7 +22,9 @@ def reroute():
 @app.route('/login')
 def signInPage():
     pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
-    return render_template('sign-in-page.html')
+    pusher_key = config.get('PUSHERDETAILS','key')
+    pusher_cluster = config.get('PUSHERDETAILS','cluster')
+    return render_template('sign-in-page.html',pusher_key=config.get('PUSHERDETAILS','key') )
 
 @app.route('/play')
 def play():
