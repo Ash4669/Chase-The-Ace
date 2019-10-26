48d8f97ea80803020ee21fbb3ae2e7b7f0da56a6
Ashley Whitehouse <ash4669@hotmail.co.uk>
2019-10-26 17:22:17 +0100

    authentication framework fully functional. Requires login to access certain pages, links disappear/ reappear depending on whether the user is authenticated. Navigation of the sit is complete. Database is fully functional and users can signup and then login in with those valid details. Alerts / prompts are arranged for users to check details to whether the login details exist or the password is incorrect.

---
+++ models.py
@@ -0,0 +1,13 @@
+# models.py
+
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    firstName = db.Column(db.String(1000))
+    lastName = db.Column(db.String(1000))
+    chaseTheAceWins = db.Column(db.Integer)
