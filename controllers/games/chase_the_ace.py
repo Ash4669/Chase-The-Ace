ddb741ea5ae0d1038b6c0fd87597ee69a0317b5c
Ashley Whitehouse <ash4669@hotmail.co.uk>
2019-11-15 15:25:14 +0000

    readme updated with more imports and another to do. config added to __init__ for sql database. pusher and config added to chase-the-ace controller and trigger in variable page. chase the age page is a copy of base.html until yeilding is done in multiple places

--- controllers/games/chase_the_ace.py
+++ controllers/games/chase_the_ace.py
@@ -1,6 +1,19 @@
 # chase_the_ace.py
 
 from flask import Blueprint, render_template
+from pusher import pusher
+import configparser
+
+config = configparser.ConfigParser()
+config.read("card_games_website/settings.conf")
+
+pusher = pusher_client = pusher.Pusher(
+  app_id  = config.get('PUSHERDETAILS','app_id'),
+  key     = config.get('PUSHERDETAILS','key'),
+  secret  = config.get('PUSHERDETAILS','secret'),
+  cluster = config.get('PUSHERDETAILS','cluster'),
+  ssl     = bool(config.get('PUSHERDETAILS','ssl'))
+)
 
 chase_the_ace = Blueprint('chase_the_ace',__name__)
 
@@ -10,4 +23,5 @@ def chase_the_ace_index():
 
 @chase_the_ace.route('/play/chase_the_ace/<int:game_id>')
 def chase_the_ace_instance(game_id):
-    return render_template('games/chase_the_ace.html', id = game_id)
+    pusher_client.trigger('my-channel', 'first-alert', {'message': 'hello world' + str(game_id)})
+    return render_template('games/chase_the_ace.html', id = game_id, pusher_key = config.get('PUSHERDETAILS','key'), pusher_cluster = config.get('PUSHERDETAILS','cluster'))
