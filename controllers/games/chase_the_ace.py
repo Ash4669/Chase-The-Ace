a86839d9f314c1fa614f0b1a1ac545e659a9df19
whitehousea <ash4669@hotmail.co.uk>
2019-12-13 17:02:21 +0000

    WIP: still figuring out redirect with socketio.on. Url for redirects to correct url but redirect function is not working correctly.

--- controllers/games/chase_the_ace.py
+++ controllers/games/chase_the_ace.py
@@ -18,12 +18,9 @@ def chase_the_ace_index():
 
 @chase_the_ace.route('/play/chase_the_ace/<game_id>')
 def chase_the_ace_instance(game_id):
+    print('http://127.0.0.1:5000/play/chase_the_ace/' + str(game_id))
     # pusher_client.trigger('my-channel', 'first-alert', {'message': 'hello world' + str(game_id)})
     return render_template('games/chase_the_ace/index.html', game_id = game_id)
-
-@chase_the_ace.route('/play/chase_the_ace/redirect<game_id>')
-def chase_the_ace_redirect(game_id):
-    redirect('http://127.0.0.1:5000/play/chase_the_ace/' + str(game_id))
 
 def generateid():
     print('Game id generated.')
@@ -33,4 +30,4 @@ def generateid():
 def generate_and_host_redirect(json):
     print('here')
     gameId = generateid()
-    return redirect(url_for('chase_the_ace.chase_the_ace_instance', game_id = 1))
+    print(url_for("chase_the_ace.chase_the_ace_instance", game_id = gameId))
