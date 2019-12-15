89610a335f947dd52c59ae8fa06f81ce803e134f
Ashley Whitehouse <ash4669@hotmail.co.uk>
2019-12-15 01:10:16 +0000

    Chase the ace now redirec ts to a generated id game page upon click of the host button. The javascript files fo r chase the ace have been moved up a folder as startpage is redundant.

--- static/js/chase_the_ace/StartPage.js
+++ static/js/chase_the_ace/StartPage.js
@@ -3,14 +3,14 @@ class StartPage extends Phaser.Scene {
         super({ key: "StartPage" });
     }
     preload() {
-      this.load.image("casinoRoom","../static/images/casinoRoom.jpg");
-      this.load.image("hostButton","../static/images/playbutton.png");
-      this.load.image("joinButton","../static/images/optionsbutton.png");
+      this.load.image("casinoRoom", "../../static/images/casinoRoom.jpg");
+      this.load.image("hostButton","../../static/images/playbutton.png");
+      this.load.image("joinButton","../../static/images/optionsbutton.png");
     }
     create() {
       var backgroundImage = this.add.image(0, 0, "casinoRoom").setOrigin(0,0);
-      var hostButton = this.add.image(300, 400, "hostButton").setOrigin(0, 0).setInteractive();
-      var joinButton = this.add.image(600, 400, "joinButton").setOrigin(0, 0).setInteractive();
+      var hostButton = this.add.image(300, 400, "hostButton").setOrigin(0, 0);
+      var joinButton = this.add.image(600, 400, "joinButton").setOrigin(0, 0);
 
       backgroundImage.setDisplaySize(1000, 600);
       hostButton.setDisplaySize(200, 100);
@@ -22,7 +22,7 @@ class StartPage extends Phaser.Scene {
 
     onHostButtonClicked() {
       var socket = io();
-      socket.emit('host game send', {data: 'I\'m connected!'});
+      socket.emit('host game send');
     }
 
     onJoinButtonClicked() {
