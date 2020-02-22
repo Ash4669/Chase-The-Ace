34c39bab81ed2d17cf4a9b9adef996bb36969032
Ashley Whitehouse <ash4669@hotmail.co.uk>
2020-02-22 18:54:50 +0000

    Moved place of db and added new table to db file to include all ChaseTheAce game states info.

--- models.py
+++ models.py
@@ -4,10 +4,17 @@ from flask_login import UserMixin
 from app import db
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    username = db.Column(db.String(100), unique=True)
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(100), unique = True)
+    username = db.Column(db.String(100), unique = True)
     password = db.Column(db.String(100))
     firstName = db.Column(db.String(1000))
     lastName = db.Column(db.String(1000))
     chaseTheAceWins = db.Column(db.Integer)
+
+class ChaseTheAce(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    roomId = db.Column(db.Integer, unique = True)
+    playerId = db.Column(db.String(100), unique = True)
+    card = db.Column(db.String(10), unique = True)
+    currentPlayer = db.Column(db.Integer)
