16b9214e45b595295d819e54e1aaeab64869903c
Ashley Whitehouse <ash4669@hotmail.co.uk>
2020-04-02 00:08:40 +0100

    restructring of files

--- classes/card.py
+++
@@ -1,3 +0,0 @@
-class Card(object):
-
-    cards = ["AceClubs","2Clubs","3Clubs","4Clubs","5Clubs","6Clubs","7Clubs","8Clubs","9Clubs","10Clubs","JackClubs","QueenClubs","KingClubs","AceSpades","2Spades","3Spades","4Spades","5Spades","6Spades","7Spades","8Spades","9Spades","10Spades","JackSpades","QueenSpades","KingSpades","AceHearts","2Hearts","3Hearts","4Hearts","5Hearts","6Hearts","7Hearts","8Hearts","9Hearts","10Hearts","JackHearts","QueenHearts","KingHearts","AceDiamonds","2Diamonds","3Diamonds","4Diamonds","5Diamonds","6Diamonds","7Diamonds","8Diamonds","9Diamonds","10Diamonds","JackDiamonds","QueenDiamonds","KingDiamonds"]
--- controllers/games/shed.py
+++
@@ -1,9 +0,0 @@
-# shed.py
-
-from flask import Blueprint, render_template
-
-shed = Blueprint('shed',__name__)
-
-@shed.route('/play/shed')
-def shed_index():
-    return render_template('games/shed/index.html')
