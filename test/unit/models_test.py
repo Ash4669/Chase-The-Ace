e152dc96df93689e759fe1d04829810515cccacf
Ashley Whitehouse <ash4669@hotmail.co.uk>
2020-05-08 15:00:13 +0100

    unit tests now working and first one passes. Initialisation done. Ready to merge in after other branch is merged in.

--- test/unit/models_test.py
+++ test/unit/models_test.py
@@ -1,4 +1,5 @@
-from ...application.models import User
+from ...application import models
+
 
 def test_new_user():
     """
@@ -7,11 +8,10 @@ def test_new_user():
     THEN check the id, email, username, password, first name, last name,
     chase the ace wins and authenticated fields are defined correctly.
     """
-    new_user = User('testEmail123@hotmail.com', 'testUsername', 'testPassword', 'testFirstName', 'TestLastName', 123)
+    new_user = models.User(email ='testEmail123@hotmail.com', username = 'testUsername', password = 'testPassword', firstName = 'testFirstName', lastName = 'testLastName', chaseTheAceWins = 123)
     assert new_user.email == 'testEmail123@hotmail.com'
     assert new_user.username == 'testUsername'
     assert new_user.password == 'testPassword'
     assert new_user.firstName == 'testFirstName'
     assert new_user.lastName == 'testLastName'
     assert new_user.chaseTheAceWins == 123
-    assert not new_user.authenticated
